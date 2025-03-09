dd040a85c95458bacaad58475720e1935ab486d7
johanBautista <jsbparra@gmail.com>
2025-03-09 19:12:14 +0100

    v3: Fix bild errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index";
 import { FontAwesomeIcon } from "./plugins/fontawesome";
 import "./assets/styles/_base.scss";
 import { createPinia } from "pinia";
